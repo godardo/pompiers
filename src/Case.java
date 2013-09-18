6ba471108a4fcef2921ff59f0619dcfb52d89e99
godardo <godardo@ensiarchi12.imag.fr>
2013-09-18 12:00:45 +0200

    debut de case

--- src/Case.java
+++ src/Case.java
@@ -8,5 +8,48 @@
  * @author godardo
  */
 public abstract class Case {
+    private String natureTerrain;
+    private double altitude;
+    private Position position;
+  
+    /**
+     * Nature of ground
+     * @return natureTerrain
+     */
+    public String getNatureTerrain(){
+        return natureTerrain;
+    }
+    
+    /**
+     * Return the position of the case
+     * @return position
+     */
+    public Position getPosition() {
+        return position;
+    }
+    
+    public abstract double getAccesibilitePiste();
+    
+    public abstract double getAccesibiliteTerrain();
+    
+    public abstract boolean estPointEau();
+    
+    public abstract double getProfondeurEau();
+    
+    public abstract boolean estEnFeu();
+    
+    public abstract boolean getIntensiteIncendie();
+    
+    public abstract double getSensibiliteEau();
+    
+    public abstract void recevoirEau(int nbLitres);
+    
+    public abstract String toStringCase();
+    
+    public abstract String toStringIncendies();
+    
+    @Override
+    public abstract String toString();
+    
     
 }
