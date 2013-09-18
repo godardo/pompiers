ba32ea27a1f7cc91969c229d4586aa005b6452a8
corteshs <corteshs@ensiarchi13.imag.fr>
2013-09-18 12:16:45 +0200

    Classes crees et classe Robot mis a jour

---
+++ src/Position.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.apoo;
+
+/**
+ *
+ * @author corteshs
+ */
+class Position {
+    
+}
--- src/Robot.java
+++ src/Robot.java
@@ -1,12 +1,109 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 
 /**
+ *The Robot class is an abstract class that has defined every services that is 
+ * logic and useful that a robot could have, as public methods.
+ * 
+ * @author 
+ * @version 
  *
- * @author godardo
  */
 public abstract class Robot {
+
+    private int reservoirCapacite;
+    private int reservoirCourant;
+    private int dureeRemplissageComplet;
+    private int dureeVidageComplet;
+    private int qteInterventionUnitaire;
+    private int dureeInterventionUnitaire;
     
+    private Position positionCourante;
+    private DonneesSimulation donneesSimu;
+    
+
+    /**
+     *
+     * @return
+     */
+    public int getDureeRemplissage() {
+        return dureeRemplissageComplet;
+    }
+
+    /**
+     *
+     * @param dir
+     * @return
+     */
+    public abstract int deplacer(Direction dir);
+
+    /**
+     *
+     * @param c
+     * @return
+     */
+    public abstract double getVitesse(Case c);
+
+    /**
+     *
+     * @param depart
+     * @param dir
+     * @return
+     */
+    public abstract double getVitesse(Case depart, Direction dir);
+
+    /**
+     *
+     * @param dir
+     * @return
+     */
+    public abstract double getVitesse(Direction dir);
+
+    /**
+     *
+     * @param c
+     * @return
+     */
+    public abstract boolean estAccesible(Case c);
+
+    /**
+     *
+     * @param depart
+     * @param dir
+     * @return
+     */
+    public abstract boolean estAccesible(Case depart, Direction dir);
+
+    /**
+     *
+     * @param dir
+     * @return
+     */
+    public abstract boolean estAccesible(Direction dir);
+
+    /**
+     *
+     * @param c
+     * @return
+     */
+    public abstract boolean peutRemplir(Case c);
+
+    /**
+     *
+     * @return
+     */
+    public abstract int remplir();
+
+    /**
+     *
+     * @param c
+     * @return
+     */
+    public abstract boolean peutIntervenirSur(Case c);
+
+    /**
+     *
+     * @param c
+     */
+    public abstract void intervenirSur(Case c);
 }
